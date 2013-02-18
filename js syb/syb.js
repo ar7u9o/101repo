8c493f3f0c5879a85e30cb291042a4d612cf1401
ar7u9o <kevin.k1252@gmail.com>
2013-02-18 02:55:03 +0100

    query functions added

--- js syb/syb.js
+++ js syb/syb.js
@@ -10,6 +10,65 @@ function everywhere(f) {
 
 }
 
+function foldl(f, data) {
+    var acc = data[0];
+    for(var i=1; i<data.length; i++) {
+        acc = f(acc, data[i]);
+    }
+    return acc;
+
+}
+
+function everything(r, f) {
+
+    return function(x) {
+        var result = gmapQ(everything(r, f), x);
+        result.push(f(x));
+        return foldl(r, result);
+    };
+
+}
+
+function gmapQ(f, val) {
+
+    if(typeof val == 'number' || typeof val == 'string') {
+        return [];
+    }
+    else {
+        var result = []
+        for(key in val) {
+            result.push(f(val[key]));
+        }
+        return result;
+    }
+
+}
+
+function mkQ(base, specific) {
+
+    return function(val) {
+        try {
+            return specific(val);
+        }
+        catch(exc) {
+            if(exc.message != 'fail') {
+                throw exc;
+            }
+            return base(val);
+        }
+
+    }
+
+}
+
+function const_(x) {
+
+    return function() {
+        return x;
+    }
+
+}
+
 function gmapT(f, val) {
 
     if(typeof val == 'number' || typeof val == 'string') {
@@ -36,6 +95,9 @@ function mkT(f) {
             return f(val);
         }
         catch(exc) {
+            if(exc.message != 'fail') {
+                throw exc;
+            }
             return val;
         }
 
@@ -66,13 +128,27 @@ function cutOne(e) {
         };
     }
     else {
-        throw new Error("wrong type");
+        throw new Error("fail");
     }
 }
+
+function getSalary(e) {
+
+    if(e.salary != null) {
+        return e.salary;
+    }
+    else {
+        throw new Error("fail");
+    }
+
+}
+
+total = everything(function (a, b) { return a+b; }, mkQ(const_(0), getSalary));
+console.log(total(company));
  
 cut = everywhere(mkT(cutOne))
 cut_company = cut(company);
-console.log(cut_company)
+console.log(total(cut_company));
 
 
 
